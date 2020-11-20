89efbfa3a0db54546f48c834e84c112988529a12
s-fuku <shintfukushima@gmail.com>
2020-11-20 13:23:10 +0900

    add synthetic-abrupt

--- real/popmove/data_processed/demo_deltacon.m
+++
@@ -1,2 +0,0 @@
-[sim, simSTD, time, timeSTD] = DeltaCon('edge', 'naive', 'GRAPHS/rKron5_1.txt', 'GRAPHS/erKron5_1.txt', 0.1)
-[sim, simSTD, time, timeSTD] = DeltaCon('edge', 'fast', 'GRAPHS/rKron5_1.txt', 'GRAPHS/rKron5_1.txt', 0.1)
